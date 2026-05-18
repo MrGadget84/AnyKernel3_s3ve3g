706597e4866defb5ce44035159f8bfe9ce8d4abb
MrGadget84 <superhyper52@gmail.com>
2026-05-18 22:33:16 +0400

    Exynos Fix

--- anykernel.sh
+++ anykernel.sh
@@ -9,7 +9,7 @@ do.devicecheck=1
 do.cleanup=1
 do.cleanuponabort=0
 do.modules=1
-dump_dtb=1
+dump_dtb=0
 device.name1=chagalllte
 device.name2=SM-T805
 supported.versions=
@@ -24,20 +24,14 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+block=/dev/block/platform/dw_mmc.0/by-name/BOOT;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=false;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
-
 # boot install
 dump_boot;
-
-if [ -f $home/dtb ]; then
-  cat $home/zImage $home/dtb > $home/zImage-dtb;
-fi
-
 write_boot;
 ## end boot install
