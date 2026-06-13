b9602e0faa81b22570f9090dc2998bdcbe8c3ebc
MrGadget84 <superhyper52@gmail.com>
2026-06-13 21:28:01 +0400

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -7,14 +7,12 @@ properties() { '
 kernel.string=
 do.devicecheck=1
 do.cleanup=1
-do.cleanuponabort=0
+do.cleanuponabort=1
 do.modules=1
-dump_dtb=1
-device.name1=s3ve3g
-device.name2=s3ve3ds
-device.name3=GT-I9301I
-device.name4=GT-I9300I
-device.name5=s3ve
+dump_dtb=0
+device.name1=a32
+device.name2=SM-A325F
+device.name3=a325f
 supported.versions=
 '; } # end properties
 
@@ -27,20 +25,16 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-patch_vbmeta_flag=false;
+patch_vbmeta_flag=true;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
 # boot install
 dump_boot;
-
-if [ -f $home/dtb ]; then
-  cat $home/zImage $home/dtb > $home/zImage-dtb;
-fi
 
 write_boot;
 ## end boot install
